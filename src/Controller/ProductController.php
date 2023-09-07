d2d2598063173dde1bd84c12f5637adf3832d0cd
wolf-wetal <wolf-wetal.ru2010@mail.ru>
2023-09-07 19:27:07 +0300

    перенос логики управления бд

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,54 +2,49 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Products;
 use App\Service\AlzaParser;
 use App\Form\ProductsType;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use App\Service\ImageUploaderService;
 
 class ProductController extends AbstractController
 {
-    private $entityManager;
+    private $imageUploader;
+    private $productsRepository;
 
-    public function __construct(EntityManagerInterface $entityManager, ImageUploaderService $imageUploader)
+    public function __construct(ImageUploaderService $imageUploader, ProductsRepository $productsRepository)
     {
-        $this->entityManager = $entityManager;
         $this->imageUploader = $imageUploader;
+        $this->productsRepository = $productsRepository;
     }
 
 
     #[Route('/create-product', name: 'create_product', methods: ['POST', 'GET'])]
     public function createProduct(Request $request, AlzaParser $alzaParser): Response
     {
+        $form = $this->createForm(ProductsType::class);
+        $form->handleRequest($request);
 
-        $form = $this->createForm(ProductsType::class); //Создаем форму на основе ProductsType
-        $form->handleRequest($request); // Обработка данных из запроса
-        $product = new Products();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $url = $form->get('alzaUrl')->getData();
+            $productData = $alzaParser->parseProductInfo($url);
 
-        if ($form->isSubmitted() && $form->isValid()) { // Проверяем на форме должан быть нажата кнопка и валидность
-            $url = $form->get('alzaUrl')->getData(); // Получаем данные
-            $productData = $alzaParser->parseProductInfo($url); //Парсим
             if ($productData) {
-
+                $product = new Products(); // Создаем новый объект Product
                 $product->setName($productData['name']);
                 $product->setPrice($productData['price']);
                 $imageFileName = $productData['image'];
-
-                $fileName = $this->imageUploader->uploadImageFromUrl($imageFileName); // Загружаем изображение по URL
-
+                $fileName = $this->imageUploader->uploadImageFromUrl($imageFileName);
                 $product->setImage($fileName);
-
                 $product->setDescription($productData['description']);
                 $product->setAlzaUrl($url);
 
-                $this->entityManager->persist($product);
-                $this->entityManager->flush();
+                $this->productsRepository->save($product);
 
                 return $this->redirectToRoute('product_list');
             } else {
@@ -62,10 +57,11 @@ class ProductController extends AbstractController
         ]);
     }
 
+
     #[Route('/', name: 'product_list')]
     public function productList(): Response
     {
-        $products = $this->entityManager->getRepository(Products::class)->findAll();
+        $products = $this->productsRepository->findAllProducts();
 
         return $this->render('products/list.html.twig', [
             'products' => $products,
@@ -73,10 +69,9 @@ class ProductController extends AbstractController
     }
 
     #[Route('/product/{id}', name: 'product_show')]
-    public function showProduct(Request $request): Response
+    public function showProduct(int $id): Response
     {
-        $product = $this->entityManager->getRepository(Products::class)->find($request->attributes->get('id'));
-
+        $product = $this->productsRepository->findProductById($id);
 
         return $this->render('products/show.html.twig', [
             'product' => $product,
@@ -84,30 +79,24 @@ class ProductController extends AbstractController
     }
 
     #[Route('/product/{id}/edit', name: 'product_edit', methods: ['POST', 'GET'])]
-    public function editProduct(Request $request): Response
+    public function editProduct(Request $request, int $id): Response
     {
-
-        $product = $this->entityManager->getRepository(Products::class)->find($request->attributes->get('id'));
-
+        $product = $this->productsRepository->findProductById($id);
         $form = $this->createForm(ProductsType::class, $product);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            try {
-                $imageFile = $form['image']->getData();
-                if ($imageFile) {
-                    $fileName = $this->imageUploader->upload($imageFile);
-                    $product->setImage($fileName);
-                }
+            $imageFile = $form['image']->getData();
 
-                $this->entityManager->flush();
-            } catch (FileException $e) {
-                $this->addFlash('error', 'Не удалось загрузить изображение.');
+            if ($imageFile) {
+                $fileName = $this->imageUploader->upload($imageFile);
+                $product->setImage($fileName);
             }
 
-            return $this->redirectToRoute('product_list');
+            $this->productsRepository->save($product);
 
+            return $this->redirectToRoute('product_list');
         }
 
         return $this->render('products/edit.html.twig', [
@@ -117,14 +106,15 @@ class ProductController extends AbstractController
     }
 
     #[Route('/product/{id}/delete', name: 'product_delete', methods: ['POST'])]
-    public function deleteProduct(Request $request): Response
+    public function deleteProduct(int $id): Response
     {
+        $product = $this->productsRepository->findProductById($id);
 
-        $product = $this->entityManager->getRepository(Products::class)->find($request->attributes->get('id'));
-
-        $this->entityManager->remove($product);
-        $this->entityManager->flush();
+        if ($product) {
+            $this->productsRepository->delete($product);
+        }
 
         return $this->redirectToRoute('product_list');
     }
+
 }
--- src/Repository/ProductsRepository.php
+++ src/Repository/ProductsRepository.php
@@ -8,11 +8,6 @@ use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Products>
- *
- * @method Products|null find($id, $lockMode = null, $lockVersion = null)
- * @method Products|null findOneBy(array $criteria, array $orderBy = null)
- * @method Products[]    findAll()
- * @method Products[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ProductsRepository extends ServiceEntityRepository
 {
@@ -21,28 +16,27 @@ class ProductsRepository extends ServiceEntityRepository
         parent::__construct($registry, Products::class);
     }
 
-//    /**
-//     * @return Products[] Returns an array of Products objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function save(Products $product)
+    {
+        $entityManager = $this->getEntityManager();
+        $entityManager->persist($product);
+        $entityManager->flush();
+    }
 
-//    public function findOneBySomeField($value): ?Products
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function delete(Products $product)
+    {
+        $entityManager = $this->getEntityManager();
+        $entityManager->remove($product);
+        $entityManager->flush();
+    }
+
+    public function findAllProducts()
+    {
+        return $this->findAll();
+    }
+
+    public function findProductById($id)
+    {
+        return $this->find($id);
+    }
 }
